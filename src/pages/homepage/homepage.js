4bc33ee0a145b7034a753d7191594b57a194afe3
mariustoft <marius.toft@icloud.com>
2020-05-20 17:46:31 +0200

    worked out the components architecture

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,19 +1,12 @@
 import React from "react";
 
-import MenuItem from "../../components/menu-item/menu-item";
 import Directory from "../../components/directory/directory";
 
 import "./homepage.scss";
 
 const HomePage = () => (
   <div className="homepage">
-    <Directory>
-      <MenuItem title="HATS" />
-      <MenuItem title="SNEAKERS" />
-      <MenuItem title="BAGS" />
-      <MenuItem title="WOMEN" />
-      <MenuItem title="MEN" />
-    </Directory>
+    <Directory />
   </div>
 );
 
